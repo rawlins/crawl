d9cb41550868019970043ba23350e1d0ab4e9b07
advil <rawlins@gmail.com>
2018-09-06 20:24:57 -0400

    actually use perl

--- .travis/deps.pl
+++ .travis/deps.pl
@@ -7,7 +7,7 @@ my @deps = qw(
 );
 
 if ($ENV{BUILD_ALL} || $ENV{CROSSCOMPILE}) {
-    git fetch --unshallow
+    retry(qw(git fetch --unshallow && echo "Building `git describe --tags`"));
     retry(qw(git submodule update --init --recursive));
 
     push @deps, qw(
